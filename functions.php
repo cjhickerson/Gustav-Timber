e4c544045460fefbf2c0170e4b78f4d3da9f6e10
Chris Hickerson <chickerson@degdigital.com>
2015-07-14 10:29:24 -0500

    add comment functionality

--- functions.php
+++ functions.php
@@ -23,11 +23,11 @@ class Gustav extends TimberSite {
     function register_taxonomies(){
         //this is where you can register custom taxonomies
     }
-	
+
 	function register_stylesheets(){
 		wp_enqueue_style('theme_styles', get_template_directory_uri().'/css/theme.min.css');
 	}
-	
+
 	function register_scripts(){
 		wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/theme.min.js', 'jquery');
 	}
@@ -37,11 +37,11 @@ class Gustav extends TimberSite {
         $context['footerMenu'] = new TimberMenu('footer-menu');
 
         $context['site'] = $this;
-		
+
 		$context['home'] = (is_home()) ? true : false;
-		
+
 		$context['custom_header'] = get_custom_header();
-		$context['settings'] = array ( 
+		$context['settings'] = array (
 			'sidebar_position' => get_theme_mod('sidebar_position'),
 			'aside_thumbnail_position' => get_theme_mod('aside_thumbnail_position'),
 			'tease_width' => get_theme_mod('tease_width'),
@@ -52,17 +52,33 @@ class Gustav extends TimberSite {
     function add_to_twig($twig){
         /* this is where you can add your own fuctions to twig */
         $twig->addExtension(new Twig_Extension_StringLoader());
-		
+
 		$function = new Twig_SimpleFunction('get_post_format', function($id) {
 			return get_post_format($id);
 		});
 		$twig->addFunction($function);
-		
+
 		$get_theme_mod = new Twig_SimpleFunction('get_theme_mod', function($id) {
 			return get_theme_mod($id);
 		});
 		$twig->addFunction($get_theme_mod);
-		
+
+        $gustav_comment_form = new Twig_SimpleFunction('gustav_comment_form', function($id) {
+            $args = array(
+                'comment_field' => '<div class="form-group"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" class="form-control" name="comment" aria-required="true"></textarea></p>',
+                'class_submit' => 'btn btn-default',
+                'title_reply' => null
+            );
+
+            ob_start();
+            comment_form($args, $id);
+            $output = ob_get_contents();
+            ob_end_clean();
+
+            return $output;
+        });
+        $twig->addFunction($gustav_comment_form);
+
         return $twig;
     }
 
